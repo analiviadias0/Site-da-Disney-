4f746ba7a77fb65d7af60f6ba2a058d2d87e46c3
analiviadias0 <analiviacruzdias142@gmail.com>
2025-09-09 15:25:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 st.title("meus projetos 😎")
 st.caption("criado com streamlit❤️")
-st.image ("https://i.pinimg.com/236x/84/ab/48/84ab48c8e42f002d254348d4b62c4608.jpg")
+st.image ("https://wallpapercave.com/wp/wp1810080.jpg")
  
 st.markdown( 
 """
-### yunk vino⚛️!
+
 *italico* \n
 **negrinto**
 """
+
 )
